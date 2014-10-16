0501a41f67d39b31caba6ad9dd81e412f252c2f0
Madhav Jha <madhavjha@gmail.com>
2014-10-16 14:49:25 -0700

    more

--- div.py
+++ div.py
@@ -1,4 +1,9 @@
 # First implementation
+# TODO: handle d = 0
+# TODO : handle neatives
+# TODO : write test cases
+# TODO : improve algo (maybe use binary search)
+
 def divide(n, d):
 	q = 0
 	while q*d < n:
